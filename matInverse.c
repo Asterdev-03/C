33902d40c98b6821be1518ef4d1a81a8156c19c9
Aswin <aswinkarat@gmail.com>
2022-01-11 23:09:52 +0530

    init

--- matInverse.c
+++ matInverse.c
@@ -28,7 +28,7 @@ float determinant(float mat[25][25], float order) {
         return (mat[0][0]);        
     else
         for (int i = 0; i < order; i++) {
-            minorMat(mat, *minor, order, i,0);
+            minorMat(mat, *minor, order, 0, i);
             det += s * (mat[0][i] * determinant(minor, order - 1));
             s = -1 * s;
         }
@@ -40,7 +40,7 @@ void minorMat(float mat[25][25],float minor[25][25], float order, int row, int c
     int m = 0, n = 0;
     for (int i = 0;i < order; i++)
         for (int j = 0 ;j < order; j++) {
-            if (i != col && j != row) {
+            if (i != row && j != col) {
                 minor[m][n] = mat[i][j];
                 if (n < (order - 2))
                     n++;
@@ -66,19 +66,18 @@ void cofactor(float mat[25][25], float order, float det) {
 // To find the Inverse of Matrix
 void inverse(float fac[25][25], float r, float det)
 {
-    int i, j;
     float adjoint[25][25], inverse[25][25];
-    for (i = 0;i < r; i++)        
-        for (j = 0;j < r; j++)        
+    for (int i = 0;i < r; i++)        
+        for (int j = 0;j < r; j++)        
             adjoint[i][j] = fac[j][i];
-    for (i = 0;i < r; i++)       
-        for (j = 0;j < r; j++)       
+    for (int i = 0;i < r; i++)       
+        for (int j = 0;j < r; j++)       
             inverse[i][j] = adjoint[i][j] / det;
 
     printf("The inverse of matrix is :\n");
-    for (i = 0;i < r; i++) {
-        for (j = 0;j < r; j++)        
-            printf("%.0f ", inverse[i][j]);           
+    for (int i = 0;i < r; i++) {
+        for (int j = 0;j < r; j++)        
+            printf("%f ", inverse[i][j]);           
         printf("\n");
     }
 }
