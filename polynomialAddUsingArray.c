8f92712b57a2fe965ddebd5c8f8b90879f4bfeba
Aswin <aswinkarat@gmail.com>
2022-01-11 23:06:01 +0530

    init

--- polynomialAddUsingArray.c
+++ polynomialAddUsingArray.c
@@ -6,38 +6,41 @@ int main() {
     // To read value for max degree and the operands of the first polynomial
     printf("Deg 1: ");
     scanf("%d", &deg1);
-    printf("Polnomial 1: ");
-    for (int coeff = 0; coeff<deg1; coeff++)
-        scanf("%d", &poly1[coeff]);
+    printf("Polynomial 1:\n");
+    for (int i = deg1; i >= 0; i-- ) {
+        printf("Coefficient of x^%d: ",i);
+        scanf("%d", &poly1[i]);
+    }
 
     // To read value for max degree and the operands of the second polynomial
     printf("Deg 2: ");
     scanf("%d", &deg2);
-    printf("Polnomial 2: ");
-    for (int coeff = 0; coeff<deg2; coeff++)
-        scanf("%d", &poly2[coeff]);
+    printf("Polnomial 2:\n");
+    for (int i = deg2; i >= 0; i-- ) {
+    printf("Coefficient of x^%d: ",i);
+        scanf("%d", &poly2[i]);
+    }
 
     // to find the highest and lowest possible degree among the plynomials
-    int highest_deg = deg1 < deg2 ? deg1 : deg2;
-    int lowest_deg = deg1 > deg2 ? deg1 : deg2;
+    int largestPolyDeg = deg1 > deg2 ? deg1 : deg2;
+    int smallestPolyDeg = deg1 < deg2 ? deg1 : deg2;
 
     // stores values of the sum of the operands
     // with same coefficients upto the lowest degree
-    for (int coeff = 0; coeff < lowest_deg; coeff++)
-        sum[coeff] = poly1[coeff] + poly2[coeff];
+    for (int i = 0; i <= smallestPolyDeg; i++)
+        sum[i] = poly1[i] + poly2[i];
 
     // stores values of remaining operands to sum
-    for (int coeff = lowest_deg; coeff < highest_deg; coeff++) {
         if(deg1 > deg2)
-            sum[coeff] = poly1[coeff];
+            for (int i = smallestPolyDeg + 1; i <= largestPolyDeg; i++) 
+                sum[i] = poly1[i];
         if(deg2 > deg1)
-            sum[coeff] = poly2[coeff];
-    }
+            for (int i = smallestPolyDeg + 1; i <= largestPolyDeg; i++) 
+                sum[i] = poly2[i];
     
     // Display sum of the polynomials
     printf("Polnomial Sum: ");
-    for (int coeff = 0; coeff < highest_deg; coeff++)
-        printf("%d ", sum[coeff]);
-
+    for (int i = largestPolyDeg; i >= 0; i--)
+        printf("%dx^%d + ",sum[i], i);
     return 0;
 }
