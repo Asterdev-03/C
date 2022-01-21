0ab1873df1fc696dd4ce4b9b46d9101853ff6daa
Aswin K <aswinkarat@gmail.com>
2022-01-21 23:14:23 +0530

    checks number is even or odd

--- even_odd.c
+++ even_odd.c
@@ -1,15 +1,15 @@
+// To check whether a number is Even or Odd using Bitwise AND
+
 #include <stdio.h>
 
-
-void even_odd(int num)
-{
+void even_odd(int num) {
+    // num & 1 return 1 if num is odd else 0 if even
     if ( !(num & 1) )
         printf("even");
     else
         printf("odd");
 }
 
-void main()
-{
+void main() {
     even_odd(5);
 }
