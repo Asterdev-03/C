734c0b79649da8f34b8b1dfc2db043ce08f4ad36
Aswin <aswinkarat@gmail.com>
2022-04-24 19:20:55 +0530

    init

--- circularQueue.c
+++ circularQueue.c
@@ -16,18 +16,19 @@ int main()
 {
     enqueue(5);
     enqueue(16);
+    enqueue(25);
+    enqueue(19);
     dequeue();
     peek();
     enqueue(71);
+    enqueue(51);
+    dequeue();
+    dequeue();
     dequeue();
     peek();
     dequeue();
-    peek();
-    dequeue();
-    enqueue(28);
-    dequeue();
-    peek();
     enqueue(25);
+    peek();
 
     return 0;
 }
@@ -38,10 +39,17 @@ void enqueue(int item)
     {
         printf("Queue Overflow\n");
         return;
-    }    
-    if (front == -1)
+    }
+    if (isEmpty() == 1)
+    {
         front = 0;
-    queue[++rear] = item;
+        queue[++rear] = item;
+    }
+    else
+    {
+        rear = (rear + 1) % MAXSiZE;
+        queue[rear] = item;
+    }
     printf("Enqueued %d\n", queue[rear]);
 }
 
@@ -52,14 +60,15 @@ void dequeue()
         printf("Queue Underflow\n");
         return;
     }
-    printf("Front Element: %d\n", queue[front]);
-    if(front == rear)
+
+    printf("%d Dequeued\n", queue[front]);
+    if (front == rear)
     {
         front = -1;
         rear = -1;
     }
     else
-        front++;
+        front = (front + 1) % MAXSiZE;
 }
 
 void peek()
@@ -69,18 +78,19 @@ void peek()
         printf("Queue Underflow\n");
         return;
     }
+    printf("Front Element: %d\n", queue[front]);
 }
 
 int isEmpty()
 {
-    if (front == -1 || front > rear)
+    if (front == -1 && rear == -1)
         return 1;
     return 0;
 }
 
 int isFull()
 {
-    if (rear == MAXSiZE - 1)
+    if (front == (rear + 1) % MAXSiZE)
         return 1;
     return 0;
 }
