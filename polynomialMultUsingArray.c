0e5fa74ec34ed826578173df63cc63fc659db8e5
Aswin <aswinkarat@gmail.com>
2022-04-24 19:40:51 +0530

    init

--- polynomialMultUsingArray.c
+++ polynomialMultUsingArray.c
@@ -1,43 +1,51 @@
-# include <stdio.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
     int poly1[20], poly2[20], mult[20], deg1, deg2;
 
     // To read value for max degree and the operands of the first polynomial
     printf("Deg 1: ");
     scanf("%d", &deg1);
-    printf("Polnomial 1: ");
-    for (int coeff = 0; coeff<deg1; coeff++)
-        scanf("%d", &poly1[coeff]);
+    printf("Polynomial 1:\n");
+    for (int i = deg1; i >= 0; i--)
+    {
+        printf("Coefficient of x^%d: ", i);
+        scanf("%d", &poly1[i]);
+    }
 
     // To read value for max degree and the operands of the second polynomial
     printf("Deg 2: ");
     scanf("%d", &deg2);
-    printf("Polnomial 2: ");
-    for (int coeff = 0; coeff<deg2; coeff++)
-        scanf("%d", &poly2[coeff]);
+    printf("Polnomial 2:\n");
+    for (int i = deg2; i >= 0; i--)
+    {
+        printf("Coefficient of x^%d: ", i);
+        scanf("%d", &poly2[i]);
+    }
 
     // to find the highest and lowest possible degree among the polynomials
     int highest_deg = deg1 < deg2 ? deg1 : deg2;
     int lowest_deg = deg1 > deg2 ? deg1 : deg2;
-    
-    for (int coeff = 0; coeff < deg1 + deg2 -1; coeff++)
+
+    for (int coeff = 0; coeff <= deg1 + deg2; coeff++)
         mult[coeff] = 0;
-    
+
     // Multiplying current term with of largest polynomial with
     // every other terms of smallest polnomial
-    for (int i = 0; i < highest_deg; i++)
-        for (int j = 0; j < lowest_deg; j++) {
+    for (int i = highest_deg; i >= 0; i--)
+        for (int j = lowest_deg; j >= 0; j--)
+        {
             if (deg1 < deg2)
                 mult[i + j] += poly1[i] * poly2[j];
             if (deg2 < deg1)
-                mult[i + j] += poly2[i] * poly1[j]; 
+                mult[i + j] += poly2[i] * poly1[j];
         }
 
     // Display sum of the polynomials
-    printf("Polnomial Sum: ");
-    for (int coeff = 0; coeff < deg1 + deg2 -1; coeff++)
-        printf("%d ", mult[coeff]);
+    printf("Polnomial Multiplicative: ");
+    for (int i = deg1 + deg2; i >= 0; i--)
+        printf("%dx^%d + ", mult[i], i);
 
     return 0;
 }
