c9782179f97b1d8ae7d05d5e3814417a1da39970
Aswin K <aswinkarat@gmail.com>
2022-10-30 12:41:15 +0530

    updated lfu.c

--- os-programs/page-replacement-algos/lfu.c
+++ os-programs/page-replacement-algos/lfu.c
@@ -2,46 +2,46 @@
 
 int main()
 {
-    int np, pages[15], nf, frame[10], pagesFaults = 0, freq[10][2] = {}, fsize = 0, pageind[10];
+    int np, pages[15], nf, frames[10], pagesFaults = 0, freq[10][2] = {}, nfreq = 0, pageIndx[10] = {};
     printf("Enter the number of pages: ");
     scanf("%d", &np);
     printf("Enter the pages reference numbers: ");
     for (int i = 0; i < np; i++)
     {
         scanf("%d", &pages[i]);
-        if (fsize == 0)
-            freq[fsize++][0] = pages[i];
+        if (nfreq == 0)
+            freq[nfreq++][0] = pages[i];
         else
         {
             int flag = 0;
-            for (int j = 0; j < fsize; j++)
+            for (int j = 0; j < nfreq; j++)
                 if (pages[i] == freq[j][0])
                 {
                     flag = 1;
                     break;
                 }
             if (flag == 0)
-                freq[fsize++][0] = pages[i];
+                freq[nfreq++][0] = pages[i];
         }
     }
 
-    printf("Enter the number of frame: ");
+    printf("Enter the number of frames: ");
     scanf("%d", &nf);
     for (int i = 0; i < nf; i++)
-        frame[i] = -1;
+        frames[i] = -1;
 
-    printf("ref string\t pages frame\n");
+    printf("ref string\t pages frames\n");
     for (int i = 0; i < np; i++)
     {
         printf("%d\t\t", pages[i]);
-
         int flag1 = 0;
+
         // pages Hit
         for (int j = 0; j < nf; j++)
-            if (frame[j] == pages[i])
+            if (frames[j] == pages[i])
             {
                 flag1 = 1;
-                for (int k = 0; k < fsize; k++)
+                for (int k = 0; k < nfreq; k++)
                     if (freq[k][0] == pages[i])
                         freq[k][1]++;
                 printf("No replacement");
@@ -50,50 +50,52 @@ int main()
         if (flag1 == 0)
         {
             pagesFaults++;
-            // pages Fault with Free frame
+            // pages Fault with Free frames
             int flag2 = 0;
             for (int j = 0; j < nf; j++)
-                if (frame[j] == -1)
+                if (frames[j] == -1)
                 {
-                    frame[j] = pages[i];
+                    frames[j] = pages[i];
                     flag2 = 1;
-                    for (int k = 0; k < fsize; k++)
+                    for (int k = 0; k < nfreq; k++)
                         if (freq[k][0] == pages[i])
                             freq[k][1]++;
 
-                    pageind[j] = i;
+                    pageIndx[j] = i;
                     break;
                 }
-            // pages Fault But No Free frame
+            // pages Fault But No Free frames
             if (flag2 == 0)
             {
-                int minfreq, frqind, pos, minpos;
+                int minfreq, frqIndx, fpos, minIndx;
+                // Finding the frame index to be changed with page[i]
                 for (int j = 0; j < nf; j++)
                 {
-                    for (int k = 0; k < fsize; k++)
+                    for (int k = 0; k < nfreq; k++)
                     {
-                        if (frame[j] == freq[k][0])
+                        if (frames[j] == freq[k][0])
                         {
-                            if ((j == 0) || (freq[k][1] < minfreq) || ((freq[k][1] == minfreq) && (pageind[j] < minpos)))
+                            if ((j == 0) || (freq[k][1] < minfreq) || ((freq[k][1] == minfreq) && (pageIndx[j] < minIndx)))
                             {
                                 minfreq = freq[k][1];
-                                minpos = pageind[j];
-                                pos = j;
-                                frqind = k;
+                                minIndx = pageIndx[j];
+                                fpos = j;
+                                frqIndx = k;
                             }
                             break;
                         }
                     }
                 }
-                frame[pos] = pages[i];
-                for (int k = 0; k < fsize; k++)
-                    if (freq[k][0] == pages[i])
-                        freq[k][1]++;
-                freq[frqind][1] = 0;
-                pageind[pos] = i;
+
+                frames[fpos] = pages[i];
+                for (int j = 0; j < nfreq; j++)
+                    if (freq[j][0] == pages[i])
+                        freq[j][1]++;
+                freq[frqIndx][1] = 0;
+                pageIndx[fpos] = i;
             }
             for (int j = 0; j < nf; j++)
-                printf("%d\t", frame[j]);
+                printf("%d\t", frames[j]);
         }
         printf("\n");
     }
