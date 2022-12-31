a7c05587dc4118a4442e60424355ac5d5a5dbf80
Aswin K <aswinkarat@gmail.com>
2022-12-31 11:27:04 +0530

    added two pass algo pgms

---
+++ os-programs/two-pass-assembler/pass1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *f1, *f2, *f3, *f4;
+    int locctr = 0, startaddr = 0;
+    char label[10], mnemonic[10], operand[10], opcode[10], machlan[10];
+
+    f1 = fopen("input.txt", "r");
+    f2 = fopen("symtab.txt", "w");
+    f3 = fopen("intermediate.txt", "w");
+
+    fscanf(f1, "%s%s%s", label, mnemonic, operand);
+
+    if (strcmp(mnemonic, "START") == 0)
+    {
+        startaddr = atoi(operand);
+        locctr = startaddr;
+        fprintf(f3, "\t\t%s\t%s\t%s\n", label, mnemonic, operand);
+    }
+
+    fscanf(f1, "%s%s", label, mnemonic);
+    while (!feof(f1))
+    {
+        fscanf(f1, "%s", operand);
+        fprintf(f3, "%d\t%s\t%s\t%s\n", locctr, label, mnemonic, operand);
+
+        if (strcmp(label, "-") != 0)
+            fprintf(f2, "%s\t%d\n", label, locctr);
+
+        f4 = fopen("optab.txt", "r");
+        fscanf(f4, "%s%s", opcode, machlan);
+
+        while (!feof(f4))
+        {
+            if (strcmp(mnemonic, opcode) == 0)
+            {
+                locctr += 3;
+                break;
+            }
+            fscanf(f4, "%s%s", opcode, machlan);
+        }
+        fclose(f4);
+
+        if (strcmp(mnemonic, "RESW") == 0)
+            locctr += 3 * atoi(operand);
+        else if (strcmp(mnemonic, "RESB") == 0)
+            locctr += atoi(operand);
+        else if (strcmp(mnemonic, "WORD") == 0)
+            locctr += 3;
+        else if (strcmp(mnemonic, "BYTE") == 0)
+        {
+            if (operand[0] == 'X')
+                locctr += 1;
+            else
+                locctr += strlen(operand) - 2;
+        }
+
+        fscanf(f1, "%s%s", label, mnemonic);
+    }
+
+    if (strcmp(mnemonic, "END") == 0)
+        printf("Program Length: %d", locctr - startaddr);
+
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+
+    return 0;
+}
---
+++ os-programs/two-pass-assembler/pass2.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *f1, *f2, *f3, *f4;
+    int locctr, symloc, flag1, flag2;
+    char label[10], mnemonic[10], operand[10], objcode[10], opcode[10], machlan[10], symname[10], str[10];
+
+    f1 = fopen("intermediate.txt", "r");
+    f2 = fopen("symtab.txt", "r");
+    f3 = fopen("optab.txt", "r");
+    f4 = fopen("output.txt", "w");
+
+    fscanf(f1, "%s%s%s", label, mnemonic, operand);
+
+    if (strcmp(mnemonic, "START") == 0)
+    {
+        fprintf(f4, "%s\t%s\t%s\n", label, mnemonic, operand);
+        fscanf(f1, "%d%s%s%s", &locctr, label, mnemonic, operand);
+    }
+
+    while (strcmp(mnemonic, "END") != 0)
+    {
+        flag1 = 0;
+
+        fscanf(f3, "%s%s", opcode, machlan);
+        while (!feof(f3))
+        {
+            if (strcmp(mnemonic, opcode) == 0)
+            {
+                flag1 = 1;
+                break;
+            }
+            fscanf(f3, "%s%s", opcode, machlan);
+        }
+
+        if (flag1 == 1)
+        {
+            flag2 = 0;
+            rewind(f2);
+            while (!feof(f2))
+            {
+                fscanf(f2, "%s%d", symname, &symloc);
+                if (strcmp(symname, operand) == 0)
+                {
+                    flag2 = 1;
+                    break;
+                }
+            }
+
+            if (flag2 == 1)
+            {
+                sprintf(str, "%d", symloc);
+                strcpy(objcode, strcat(machlan, str));
+            }
+        }
+        else if (strcmp(mnemonic, "WORD") == 0)
+            strcpy(objcode, operand);
+        else if (strcmp(mnemonic, "BYTE") == 0)
+        {
+            sprintf(str, "%d", operand[2]);
+            strcpy(objcode, str);
+        }
+        else
+            strcpy(objcode, "\0");
+
+        fprintf(f4, "%s\t%s\t%s\t%d\t%s\n", label, mnemonic, operand, locctr, objcode);
+        fscanf(f1, "%d%s%s%s", &locctr, label, mnemonic, operand);
+    }
+    fprintf(f4, "%s\t%s\t%s\t%d\n", label, mnemonic, operand, locctr);
+
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+
+    return 0;
+}
