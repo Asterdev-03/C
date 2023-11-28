f2525f5951c5a556a3c3711a6644fc9169eac35b
Aswin K <aswinkarat@gmail.com>
2023-11-28 19:26:28 +0530

    Create exp4.c

---
+++ lab/exp4.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+int isKeyword(char buffer[])
+{
+	char keywords[32][10]={"void","break","char","int","float"};
+	int flag=0;
+	for(int i=0;i<32;i++)
+	{
+		if(strcmp(keywords[i],buffer)==0)
+		{
+			flag=1;
+			break;
+		}
+	}
+	return flag;
+}
+int main()
+{
+	char ch,buffer[15],operators[]="+-*/%=";
+	FILE*fp;
+	int i,j=0;
+	fp=fopen("program.txt","r");
+	
+	while((ch==fgetc(fp))!=EOF)
+	{
+		for(i=0;i<6;++i)
+		{
+			if(ch==operators[i])
+			printf("%c is operator \n",ch);
+		}
+		if(isalnum(ch))
+		{
+			buffer[j++]=ch;
+		}
+		else if((ch==' '||ch=='\n')&&(j!=0))
+		{
+			buffer[j]='\O';
+			j=0;
+			if(isKeyword(buffer)==1)
+			printf("%s is keyword\n",buffer);
+			else
+			printf("%s is indentifier \n",buffer);
+		}
+	}
+	return 0;
+}
